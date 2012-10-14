7d28b5eb74271229df004c7d3f33d5bae232680a
clux <analsandblaster@gmail.com>
2012-10-14 15:56:11 +0100

    middleware example

--- examples/express.js
+++ examples/express.js
@@ -1,6 +1,7 @@
-var makeMiddleware = function (l) {
-  return function (req, res, next) {
-    l.trace(req.method, req.url.toString());
-    next();
-  };
-};
+var l = require('logule').init(module, '<-');
+
+// simple logger with `<-` namespace
+app.use(function (req, res, next) {
+  l.trace(req.method, req.url.toString());
+  next();
+});
