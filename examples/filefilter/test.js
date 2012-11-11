b73ed5165d81040323759b871a964f72fbed4490
clux <analsandblaster@gmail.com>
2012-11-11 18:49:09 +0000

    expand on filefilter example

--- examples/filefilter/test.js
+++ examples/filefilter/test.js
@@ -1,5 +1,6 @@
 var l = require('../../').init(module);
 
+// running this appends 10 lines (== num log calls) to output.txt
 l.sub('Ns').info('hi thar').warn('bla%s', "aah");
 l.error('fwooarst %j, likeit', {a:2});
 l.zalgo('this is bad, really bad');
@@ -10,9 +11,26 @@ l.error('arst\\n\n arst')
 l.line('here i am');
 l.sub('WTF').line("where is this? %d ?", 12)
 
-/*
+return; // rest of file instructional only
+
+// then can command line filter the output
 $ cat output.txt | grep error
 {"time":"2012-11-11T18:28:58.771Z","level":"error","namespaces":[],"message":"fwooarst {\"a\":2}, likeit"}
 {"time":"2012-11-11T18:28:58.773Z","level":"error","namespaces":[],"message":"arst\\n\n arst"}
-*/
 
+
+// or we can process it with short scripts:
+var fs = require('fs');
+var file = fs.readFileSync('./output.txt').toString();
+var lines = file.split('\n').slice(0, -1); // remove trailing newline
+lines.length; // 10 (equal to number of calls despite having \n in messages)
+var last = JSON.parse(lines[lines.length-1]);
+last;
+{ time: '2012-11-11T18:28:58.773Z',
+  level: 'line',
+  namespaces: [ 'WTF' ],
+  line: 'test.js:11',
+  message: 'where is this? 12 ?' }
+
+// default date serialization can be recovered into a Date instance
+new Date(last.time).toLocaleTimeString(); // '18:28:58'
