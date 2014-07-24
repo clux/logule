2def59685b939f4b692f3fe1eba8e6a78e7e81d9
clux <analsandblaster@gmail.com>
2014-07-24 19:11:08 +0100

    get emitter tests online by changing execution cwd to test dir

--- test/emitter.js
+++ test/emitter.js
@@ -1,4 +1,3 @@
-/*
 var logule = require('../')
 var l = logule.init(module, "emitterTest")
 var emitter = logule.emitter
@@ -18,7 +17,7 @@ exports.exports = function (t) {
     t.ok(stack.length > len, "this emitted a log");
 
     var el = stack[stack.length-1];
-    t.type(el.message, 'string', "last thing is a string");
+    t.equal(typeof el.message, 'string', "last thing is a string");
     t.equal(dye.stripColors(el.message), el.message, "no colors from " + lvl);
     t.deepEqual(el.namespaces.map(dye.stripColors), el.namespaces, "namespaces");
     t.equal(el.namespaces[0], "emitterTest", "inited with a namespace");
@@ -32,4 +31,3 @@ exports.exports = function (t) {
 
   t.done();
 };
-*/
